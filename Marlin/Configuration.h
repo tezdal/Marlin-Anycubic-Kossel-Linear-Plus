f866a6b308ee4f80ca7ca78fd389f9338575ab30
Unknown <jens.dunzweiler@tavios.de>
2018-08-02 16:16:54 +0200

    Fixed board+fan

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -128,7 +128,7 @@
 // The following define selects which electronics board you have.
 // Please choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_TRIGORILLA_14
+  #define MOTHERBOARD 33
 #endif
 
 // Optional custom name for your RepStrap or other custom machine
